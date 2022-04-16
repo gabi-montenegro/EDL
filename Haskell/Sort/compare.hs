efd4d9985f0196ba205d8522f403fb37d421cbf8
Gabriella Montenegro <43149651+gabi-montenegro@users.noreply.github.com>
2022-04-16 11:54:28 -0300

    Update compare.hs

--- Haskell/Sort/compare.hs
+++ Haskell/Sort/compare.hs
@@ -1,9 +1,3 @@
-import Data.List
-
-myodd :: Integer -> Bool
-myodd n = n `rem` 2 /= 0
-
-
 myCompare :: (Ord chave, Ord nome) => (chave, nome) -> (chave,nome) -> Ordering  
 myCompare (c1,n1) (c2, n2) 
      | c1 > c2     = GT  
