b8bef9a33befc14139e37835b00a2cca3b1cede5
gabriella <gabrielamontenegro2@gmail.com>
2022-04-18 17:36:36 -0300

    listas dentro de lista

--- Haskell/Listas/list_of_list.hs
+++ Haskell/Listas/list_of_list.hs
@@ -1,37 +1,26 @@
-nomes :: [[String]]
-alturas :: [[Float]]
-pesos :: [[Float]]
+
+pessoas :: [[Float]]
+pessoas = [[1.65, 56], [1.43, 42], [1.70, 61]]
 
 
-nomes = [["maria"], ["joao"], ["gabi"]]
-alturas = [[1.65], [1.43], [1.70]]
-pesos = [[56], [42], [61]]
+
+alturaDup = map (\[a,b] -> a^2) pessoas --Transforma a lista de listas de altura em uma lista de altura ao quadrado
+
+alturaDupRound = map (\a -> round a) alturaDup--arredonda as alturas duplicadas
+
+pesos = map (\[a,b] -> b) pessoas --mapeando somente os pesos
 
 
-listaAlturaDup = map (\[a] -> a^2) alturas --Transforma a lista de listas de altura em uma lista de altura ao quadrado
-
-listaPeso = map (\[p] -> p) pesos --Transforma a lista de lista de pesos em uma lista simples de pesos
-
---Funcao zip' concatena cada elemento de duas listas em uma lista de tuplas.
-zip' :: [a] -> [b] -> [(a, b)]
-zip' xs     []     = []
-zip' []     ys     = []
-zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
-
-listaAltPeso = zip' listaAlturaDup listaPeso --[(altura1, peso1),(altura2, peso2),...,(alturaN, pesoN)]
+listaAltPeso = zip alturaDup pesos --[(altura1, peso1),(altura2, peso2),...,(alturaN, pesoN)] : lista de tuplas
 
 imc = map(\(a,p) -> a/p) listaAltPeso --Calculo do IMC
 
 
-
-listaAltura = map(\[a] -> a) alturas --Transforma lista de lista em uma simples lista 
-
+listaAltura = map(\[a,b] -> a) pessoas --Transforma lista de lista em uma simples lista 
 maiorAltura = foldl max 0 listaAltura -- Seleciona a maior altura
 
-listaNome = map (\[n] -> n) nomes -- Transforma lista de lista em uma simples lista 
-listaNomeAlt = zip' listaNome listaAltura -- Concatena a lista de nomes e lista de alturas em uma tupla
 
-nomeMaiorAlt = filter (\(nome, alt) -> alt == maiorAltura) listaNomeAlt -- Filtra nome e altura de quem é o mais alto
+idadeMaiorAlt = filter (\[alt, idade] -> alt == maiorAltura) listaNomeAlt -- Filtra altura e idade da maior altura da lista
 
 
 
