3f22953c2a0ae484052987185cacaece88de3f3c
gabriella <gabriellamontenegro2@gmail.com>
2022-04-28 11:52:03 -0300

    raizes de uma equacao quadratica

--- Haskell/Lists/equation.hs
+++ Haskell/Lists/equation.hs
@@ -1,13 +1,13 @@
 l1 :: [[Int]]
-l1 = [[2,2,3], [1,0,0], [2,-16,-18]]
+l1 = [[2,-7,3], [2,2,1], [1,5,-14]]
 
-calcula :: Int -> Int -> Int ->  [Int]
+calcula :: Int -> Int -> Int ->  [Float]
 
 
 calcula a b c = if delta < 0 then [] else if delta == 0 then [z] else [r1,r2] 
-                where r1 = (- b + (floor (raizQuadrada))) `div` (2*a)
-                      r2 = (- b - (floor (raizQuadrada))) `div` (2*a)
-                      z = -b `div` (2*a)
+                where r1 = (- (fromIntegral b) + raizQuadrada) / fromIntegral (2*a)
+                      r2 = (- (fromIntegral b) - raizQuadrada) / fromIntegral(2*a)
+                      z = - (fromIntegral b) / fromIntegral (2*a)
                       deltaFloat =  fromIntegral delta :: Float
                       delta = b * b - (4 * a * c)
                       raizQuadrada = sqrt deltaFloat
