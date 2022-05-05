4583d8754fdf615783bf8682e93eaf574b96310a
gabriella <gabriellamontenegro2@gmail.com>
2022-05-05 09:39:17 -0300

    personagem de RPG com data

--- Haskell/Lists/personagem.hs
+++ Haskell/Lists/personagem.hs
@@ -1,11 +1,11 @@
 {- Mago: nome, altura, idade, magia do mago 
-   Guerreiro: nome, altura, idade, ataque e defesa do guerreiro
+   Guerreiro: nome, altura, idade e ataque do guerreiro
    Sacerdote: nome, altura, idade, reza do Sacerdote
 -}
-data Personagem = Mago String Float Int Int | Guerreiro String Float Int Int Int | Sacerdote String Float Int Bool deriving (Show)
+data Personagem = Mago String Float Int Int | Guerreiro String Float Int Int | Sacerdote String Float Int Bool deriving (Show)
 
 p1 :: Personagem 
 p1 = Mago "joao" 1.45 12 68
 
 p2 :: Personagem 
-p2 = Guerreiro "gabi" 1.71 23 45 32
+p2 = Guerreiro "gabi" 1.71 23 45 
